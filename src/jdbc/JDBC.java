82c488d2405495d7d0055384a4b07c8014e6dd25
Devin Daher <ddaher@usc.edu>
2018-04-11 18:20:32 -0700

    Add files via upload

--- src/jdbc/JDBC.java
+++ src/jdbc/JDBC.java
@@ -1,6 +1,5 @@
 package jdbc;
 
-import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -23,19 +22,16 @@ public class JDBC {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/collage?user=root&password=root&useSSL=false");
 			st = conn.createStatement();
-			ps = conn.prepareStatement("SELECT * FROM UserInfo WHERE username=? and passwords=? and images=?");
+			ps = conn.prepareStatement("SELECT * FROM UserInfo WHERE username=? and passwords=?");
 			ps.setString(1, "daher");
 			ps.setString(2, "devin2");
-			System.out.println ("here");
-			ps.setString(3, null);
+			//boolean wahtever= login("lkj","113414");
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				String user = rs.getString("username");
 				String pass = rs.getString("passwords");
-				Blob images = rs.getBlob("images");
 				System.out.println ("username = " + user);
 				System.out.println ("password = " + pass);
-				System.out.println ("images = " + images);
 			}
 		} catch (SQLException sqle) {
 			System.out.println ("SQLException: " + sqle.getMessage());
@@ -60,7 +56,6 @@ public class JDBC {
 			}
 		}
 	}
-	
 	public boolean login(String userName, String password) {
 		Connection conn = null;
 		Statement st = null;
@@ -75,38 +70,21 @@ public class JDBC {
 			ps = conn.prepareStatement("SELECT * FROM UserInfo WHERE username=? and passwords=?");
 			ps.setString(1, userName);
 			ps.setString(2, password);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				String user = rs.getString("username");
-				String pass = rs.getString("passwords");
-				System.out.println ("username = " + user);
-				System.out.println ("password = " + pass);
+ 			rs = ps.executeQuery();
+ 			while (rs.next()) {
+ 				String user = rs.getString("username");
+ 				String pass = rs.getString("passwords");
+ 				System.out.println ("username = " + user);
+ 				System.out.println ("password = " + pass);
 				return true;
-			}
-		} catch (SQLException sqle) {
-			System.out.println ("SQLException: " + sqle.getMessage());
-		} catch (ClassNotFoundException cnfe) {
-			System.out.println ("ClassNotFoundException: " + cnfe.getMessage());
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (st != null) {
-					st.close();
-				}
-				if (ps != null) {
-					ps.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException sqle) {
-				System.out.println("sqle: " + sqle.getMessage());
-			}
-		}
+ 			}
+ 		} catch (SQLException sqle) {
+ 			System.out.println ("SQLException: " + sqle.getMessage());
+ 		}catch (ClassNotFoundException cnfe) {
+ 			System.out.println ("ClassNotFoundException: " + cnfe.getMessage());
+ 		}
 		return false;
-	}
+ 	}
 	public boolean signup(String username, String password) {
 		//check sql database for this username and if password matches
 		Connection conn = null;
@@ -161,31 +139,31 @@ public class JDBC {
 		return true;
 	}
 }
-
-//this is for potential change in BLOB prepared statement
-/*
-//public void insert() throws Exception {
-Connection conn = null;
-PreparedStatement ps = null;
-
-InputStream is = null;
-try {
-    conn = this.connection.getConnection();
-    String sql = "INSERT INTO Table (image) VALUES (?)";
-
-    ps = conn.prepareStatement(sql);
-    if (this.file != null && this.file.canRead()) {
-        is = new BufferedInputStream(new FileInputStream(this.file));
-        ps.setBinaryStream(1, is, (int) this.file.length());
-    } else {
-        ps.setNull(1, Types.BLOB);
-    }
-} catch (Exception e) {
-    LOG.error("", e);
-    throw e;
-} finally {
-    FileUtil.close(is);
-    DAOUtil.close(conn);
-}
-}
-*/
+		
+		//this is for potential change in BLOB prepared statement
+		/*
+		+//public void insert() throws Exception {
+		+Connection conn = null;
+		+PreparedStatement ps = null;
+		+
+		+InputStream is = null;
+		+try {
+		+    conn = this.connection.getConnection();
+		+    String sql = "INSERT INTO Table (image) VALUES (?)";
+		+
+		+    ps = conn.prepareStatement(sql);
+		+    if (this.file != null && this.file.canRead()) {
+		+        is = new BufferedInputStream(new FileInputStream(this.file));
+		+        ps.setBinaryStream(1, is, (int) this.file.length());
+		+    } else {
+		+        ps.setNull(1, Types.BLOB);
+		+    }
+		+} catch (Exception e) {
+		+    LOG.error("", e);
+		+    throw e;
+		+} finally {
+		+    FileUtil.close(is);
+		+    DAOUtil.close(conn);
+		+}
+		 }
+		+*/
