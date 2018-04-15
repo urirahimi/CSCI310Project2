54425468afaaebbec9877c6a0cea7fdb8b341c86
Devin Daher <ddaher@usc.edu>
2018-04-15 16:51:34 -0700

    Add files via upload

--- src/junitTest/registrationTest.java
+++ src/junitTest/registrationTest.java
@@ -1,15 +1,18 @@
 package junitTest;
-
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import jdbc.db_connection;
 
 public class registrationTest {
 
 	@Test
 	public void register() {
-		AssertTrue(!Register.register("Vijay", "123")); //username already exists
-		AssertTrue(Register.register("Vij", "123")); //username does not exist, success
+		db_connection sqlDB = new db_connection();
+		Assert.assertTrue(!sqlDB.signup("daher", "devin2"));
+		Assert.assertTrue(sqlDB.signup("Vijayyyyy", "123"));
+		//AssertTrue(!Register.register("daher", "devin2")); //username already exists
+		//AssertTrue(Register.register("Vij", "123")); //username does not exist, success
 	}
 
 }
