4841b5496a929008e3ff1611d63cd95acf7889eb
Eddo W. Hintoso <eddo.hintoso+github@gmail.com>
2018-04-24 13:15:46 -0700

    Black box test loading symbol and insufficient images

--- bbtests/features/step_definitions/collage_page_appearance_steps.rb
+++ bbtests/features/step_definitions/collage_page_appearance_steps.rb
@@ -5,11 +5,16 @@ MAIN_LINK = "http://localhost:8080/CSCI310/landing.jsp"
 COLLAGE_LINK = "http://localhost:8080/CSCI310/search"
 browser = Watir::Browser.new
 
-Given("I am looking at the collage page") do
+Given("I submitted a valid topic on the main page") do
   browser.goto MAIN_LINK
   topic_input_box = browser.text_field(:id, "topic-input")
   topic_input_box.set("dogs") # valid input with sufficient images
   browser.button(:id, "topicSubmit").click
+end
+
+Then("A loading symbol should appear until I see the collage page") do
+  loading_symbol = browser.div(:id, "loading")
+  expect(loading_symbol.style("display")).to eq("none")
   expect(browser.url).to eq(COLLAGE_LINK)
 end
 
@@ -33,16 +38,46 @@ Then("The input box for topic has placeholder text") do
   expect(input_box.placeholder.empty?).to eq(false)
 end
 
-Then("A rectangle that displays miniaturized images of previous collages generated.") do
+Then("A rectangle that displays miniaturized images of previous collages generated is shown") do
   prev_collages_div = browser.div(:id, "previous-collages")
   expect(prev_collages_div.exists?).to eq(true)
 end
 
-Then("A collage is shown if there are sufficient images.") do
+Then("A collage is shown") do
   collage_image = browser.img(:id, "collage-image")
   expect(collage_image.exists?).to eq(true)
 end
 
-Then("An error message is shown if there are insufficient images.") do
-  browser.close
+Given("I submitted an invalid topic on the main page") do
+  browser.goto MAIN_LINK
+  topic_input_box = browser.text_field(:id, "topic-input")
+  topic_input_box.set("daflk;sfdsfjrianvknelirjflq3n") # invalid input with insufficient images
+  browser.button(:id, "topicSubmit").click
+end
+
+Then("A loading symbol should still appear until I see the collage page") do
+  loading_symbol = browser.div(:id, "loading")
+  expect(loading_symbol.style("display")).to eq("none")
+  expect(browser.url).to eq(COLLAGE_LINK)
+end
+
+Then("An error message is shown") do
+  error_message = browser.img(:id, "collage-page")
+  expect(error_message.alt).to eq("Insufficient images found.")
+end
+
+Then("All UI elements besides the collage is the same") do
+  # export button
+  export_collage_button = browser.button(:id, "export-button")
+  expect(export_collage_button.exists?).to eq(true)
+  # save button
+  save_collage_button = browser.button(:id, "save-button")
+  expect(save_collage_button.exists?).to eq(true)
+  # input box
+  input_box = browser.text_field(:id, "input")
+  expect(input_box.exists?).to eq(true)
+  expect(input_box.placeholder.empty?).to eq(false)
+  # previous collages
+  prev_collages_div = browser.div(:id, "previous-collages")
+  expect(prev_collages_div.exists?).to eq(true)
 end
